92d52693b58b5530f0b4c190faf4923764360394
SyedMash <mashrtx7@gmail.com>
2025-07-26 07:41:24 -0700

    Refactor Features component to implement GSAP animations and improve layout structure

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,19 +1,46 @@
-import React from "react";
+"use client";
+
+import React, { useRef } from "react";
+
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 import Bounded from "@/components/Bounded";
 
 const Features = () => {
+  const featureRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      gsap.to(".boxes", {
+        y: 0,
+        opacity: 1,
+        ease: "expo.in",
+        stagger: 0.45,
+        scrollTrigger: {
+          trigger: featureRef.current,
+          start: "top 80%",
+          end: "bottom top",
+          markers: true,
+        },
+      });
+    },
+    { scope: featureRef }
+  );
+
   return (
-    <section className="bg-gray-100">
+    <section className="min-h-screen" ref={featureRef}>
       <Bounded>
         <h3 className="font-rubik-sans capitalize text-3xl lg:text-4xl xl:text-5xl">
           Key Features
         </h3>
         <p className="text-custom-subtext">Why you&apos;ll love this journal</p>
 
-        <div className="min-h-screen mt-12">
-          <div className="flex size-full flex-col gap-6 justify-between">
-            <div className="flex-1 bg-black/9 h-[33vh] rounded-xl p-4 flex-col flex items-end justify-end">
+        <div className="h-screen lg:h-screen mt-12">
+          <div className="flex flex-col gap-6 h-full w-full justify-between">
+            <div className="flex-1 bg-red-600 h-2/4 rounded-xl p-4 flex-col flex items-end justify-end boxes translate-y-12 opacity-0">
               <span className="text-2xl font-semibold font-rubik-sans text-custom-main-text">
                 ✏️ Rich Text Editor
               </span>
@@ -21,8 +48,8 @@ const Features = () => {
                 A beautiful, distraction-free space to write.
               </p>
             </div>
-            <div className="flex flex-col lg:flex-row gap-6 h-[33vh]">
-              <div className="flex-1 bg-green-400 rounded-xl p-4 flex-col flex items-end justify-end">
+            <div className="flex flex-col lg:flex-row gap-6 h-2/4">
+              <div className="flex-1 bg-green-400 rounded-xl p-4 flex-col flex items-end justify-end boxes translate-y-12 opacity-0">
                 <span className="text-2xl font-semibold font-rubik-sans text-custom-main-text">
                   🤖 Smart Ai Summaries
                 </span>
@@ -30,7 +57,7 @@ const Features = () => {
                   Instant insights, reflections, affirmations.
                 </p>
               </div>
-              <div className="flex-1 bg-yellow-300 rounded-xl p-4 flex-col flex items-end justify-end">
+              <div className="flex-1 bg-yellow-300 rounded-xl p-4 flex-col flex items-end justify-end boxes translate-y-12 opacity-0">
                 <span className="text-2xl font-semibold font-rubik-sans text-custom-main-text">
                   📊 Mood Tracker
                 </span>
@@ -38,20 +65,12 @@ const Features = () => {
                   See how you’re feeling over time.
                 </p>
               </div>
-              <div className="flex-1 bg-pink-500 rounded-xl p-4 flex-col flex items-end justify-end">
+              <div className="flex-1 bg-pink-500 rounded-xl p-4 flex-col flex items-end justify-end boxes translate-y-12 opacity-0">
                 <span className="text-2xl font-semibold font-rubik-sans text-custom-main-text">
                   🔒 Private & Secure
                 </span>
                 <p className="text-custom-subtext">Only you have access</p>
               </div>
-            </div>
-            <div className="flex-1 bg-blue-400 h-[33vh] rounded-xl p-4 flex-col flex items-end justify-end">
-              <span className="text-2xl font-semibold font-rubik-sans text-custom-main-text">
-                📆 Daily Streaks
-              </span>
-              <p className="text-custom-subtext">
-                Stay motivated, one day at a time.
-              </p>
             </div>
           </div>
         </div>
